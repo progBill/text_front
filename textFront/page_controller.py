8b99b037bd1f786f483e592c9ca1c54afbd603b6
Bill <billebeling@gmail.com>
2014-09-15 17:49:55 -0400

    change layout to pagecontroller to be clearer

---
+++ textFront/page_controller.py
@@ -0,0 +1,15 @@
+
+# key: page location
+# [0] topnav
+main_layout={
+"Main": ["Stuff","More Stuff", "Even More Stuff"],
+    "About": [],
+    "X from Y": [],
+    "Current Text": [],
+}
+
+
+if __name__ == '__main__':
+    print main_layout.keys()
+
+
