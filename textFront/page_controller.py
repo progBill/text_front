9e8210f15170f4e4085b739d3c66ecefc920ae78
Bill <billebeling@gmail.com>
2014-09-20 17:58:07 -0400

    major front end changes

--- textFront/page_controller.py
+++ textFront/page_controller.py
@@ -1,15 +1,26 @@
+from os import listdir, path
+
+lib_path = path.join('.','textFront','library')
+#lib_path = path.join('.','library')
+
 
 # key: page location
 # [0] topnav
 main_layout={
-"Main": ["Stuff","More Stuff", "Even More Stuff"],
-    "About": [],
-    "X from Y": [],
-    "Current Text": [],
+"welcome": listdir(lib_path),
+"About": [],
+"X from Y": [],
+"Current Text": [],
+}
+
+tasks={
+    'Word Distances'
 }
 
 
 if __name__ == '__main__':
     print main_layout.keys()
+    print main_layout['welcome']
+
 
 
--- textFront/views.py
+++ textFront/views.py
@@ -1,21 +1,26 @@
 from textFront import app
-from flask import render_template
-from page_controller import main_layout
+from flask import render_template, jsonify, request
+from page_controller import main_layout, tasks
+from os import path
 
 @app.route('/')
 @app.route('/index')
-def index(menus=main_layout.keys()):
-	return render_template("welcome.html", menus=menus)
+def index(page_key='welcome'):
+    template = '%s.html' % page_key
+    links = [dict(label=r.split('.')[0], link=r) for r in (main_layout[page_key])]
+    menus=sorted(main_layout.keys())
+    ts=[dict(name=x) for x in  sorted(tasks)][0]
+    return render_template(template, menus=menus, texts=links, tasks=ts)
 
-@app.route('/get_text/<int:text_id>')
-def get_text(text_id):
-    text = '\n%s\n' % text_id
-    return 'texted: %s' % text
 
-@app.route('/putText')
-def put_text(txt):
-    pass    
+@app.route('/get_text')
+def get_text():
 
+    text_id = request.args.getlist('text_id')[0]
+    text_loc = path.join('.', 'textFront','library', text_id)
+    with open(text_loc, 'r') as f:
+        text = f.read()
+    return jsonify({"textbody":text})
 
 
 
