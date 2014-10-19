0b492b05cf64b54a3b806a3b88fa3050803f6ba5
Bill <billebeling@gmail.com>
2014-10-19 14:27:20 -0400

    got white / black list working

--- textFront/page_controller.py
+++ textFront/page_controller.py
@@ -3,7 +3,6 @@ from os import listdir, path
 lib_path = path.join('.', 'textFront', 'library')
 
 # this list is the top navigation for the page
-# each page should be a Single Page Application
 top_nav =['Home',"About"]
 
 # associates resources needed by each page
@@ -22,5 +21,4 @@ task_dependencies={
     'Word Locations':{},
 }
 
-if __name__ == '__main__':
-    print main_layout.keys()
+
