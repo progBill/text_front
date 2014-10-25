c2b38d440e8a0639499fa21434eec6fcca3b1bdd
Bill <billebeling@gmail.com>
2014-10-25 14:44:10 -0400

    improved ajax, added word frequencies chunks

--- textFront/static/Frequencies.js
+++ textFront/static/Frequencies.js
@@ -95,7 +95,6 @@ Frequencies.prototype.displayLongest= function(){
  * Utilities and Setup 
  */
 
-
 Frequencies.prototype.paramHapaxes = function(){
     frequencies.hideElem('.display');
     frequencies.showElem('.params');
@@ -115,12 +114,43 @@ function turnTestsOn(){
     frequencies.onClickByClass('.jsHapaxes', frequencies.paramHapaxes);
     frequencies.onClickByClass('.jsAll-Tokens', frequencies.displayAll);
     frequencies.onClickByClass('.jsLong-Words', frequencies.paramLongest);
-//    frequencies.onClickByClass('.jsLong-Words', frequencies.displayLongest);
+    frequencies.getJson('/get_word_freq_in_chunk','CHUNKS','and');
 };
 
 store.subscribe('LIB_READY', turnTestsOn);
 frequencies.getJson('/get_word_count', 'LIB_READY');
 frequencies.showElem('.display');
+////////////// testing charts
 
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+
+
+// Get the context of the canvas element we want to select
+var ctx = document.getElementById("chart").getContext("2d");
+var myNewChart = new Chart(ctx).Line(data);
 
 
--- textFront/texter/wrappers.py
+++ textFront/texter/wrappers.py
@@ -7,9 +7,6 @@ def get_tokens(s):
     '''Returns a list of word tokens'''
     return word_tokenize(s.encode('utf-8'))
 
-def get_longest_words(s):
-    pass
-
 def lexical_diversity(s):
     '''Provides a measure of lexical diversity'''
     num_tokens = len(get_tokens(s))
@@ -30,6 +27,14 @@ def get_longest_words(s, x=10):
     long_words= sorted(tokens, key=lambda l: len(l))
     return long_words[x*-1:]
 
+
+def get_chunked_word_frequency(s,w, chunk_size=300):
+    '''takes a text and a word, returns a list of frequencies'''
+    token_list = get_tokens(s)
+    num_tokens = len(token_list)
+    freqs = [token_list[i:i+chunk_size].count(w) for i in range(0, len(token_list), chunk_size)]
+
+    return freqs
 
 #########################
 ##  Just a way to test ##    
@@ -53,8 +58,11 @@ class TestNLTKFunctions(unittest.TestCase):
         self.assertTrue('Freqy' in fd)
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestNLTKFunctions)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+#    suite = unittest.TestLoader().loadTestsFromTestCase(TestNLTKFunctions)
+#    unittest.TextTestRunner(verbosity=2).run(suite)
+    s="not let belief take hold of him Touching this dreaded sight, twice seen of us: Therefore I have entreated him along With us to watch the minutes of this night; That if again this apparition come, He may approve our eyes and speak to it. HORATIO Tush, tush, 'twill not appear. BERNARDO Sit down awhile; And let us once again assail your ears, That are so fortified against our story What we have two nights seen. HORATIO Well, sit we down, And let us hear Bernardo speak of this. BERNARDO Last night of all, When yond same star that's westward from the pole Had made his course to illume that part of heaven Where now it burns, Marcellus and myself, The bell then beating one,-- Enter Ghost MARCELLUS Peace, break thee off; look, where it comes again! BERNARDO In the same figure, like the king that's dead. MARCELLUS Thou art a scholar; speak to it, Horatio. BERNARDO Looks it not like the king? mark it, Horatio. HORATIO Most like: it harrows me with fear and wonder. BERNARDO It would be spoke to. MARCELLUS Question it, Horatio. HORATIO What art thou that usurp'st this time of night, Together with that fair and warlike form In which the majesty of buried Denmark Did sometimes march? by heaven I charge thee, speak! MARCELLUS It is offended. BERNARDO See, it stalks away! HORATIO Stay! speak, speak! I charge thee, speak! Exit Ghost MARCELLUS 'Tis gone, and will not answer. BERNARDO How now, Horatio! you tremble and look pale: Is not this something more than fantasy? What think you on't? HORATIO Before my God, I might not this believe Without the sensible and true avouch Of mine own eyes. MARCELLUS Is it not like the king? HORATIO As thou art to thyself: Such was the very armour he had on When he the ambitious Norway combated; So frown'd he once, when, in an angry parle, He smote the sledded Polacks on the ice. 'Tis strange. MARCELLUS Thus twice before, and jump at this dead hour, With martial stalk hath he gone by our watch. HORATIO In what particular thought to work I know not; But in the gross and scope of my opinion, This bodes some strange eruption to our state. MARCELLUS Good now, sit down, and tell me, he that knows, Why this same strict and most observant watch So nightly toils the subject of the land, And why such daily cast of brazen cannon, And foreign mart for implements of war; Why such impress of shipwrights, whose sore task Does not divide the Sunday from the week; What might be toward, that this sweaty haste Doth make the night joint-labourer with the day: Who is't that can inform me? HORATIO That can I; At least, the whisper goes so. Our last king, Whose image even but now appear'd to us, Was, as you know, by Fortinbras of Norway, Thereto prick'd on by a most emulate pride, Dared to the combat; in which our valiant Hamlet-- For so this side of our known world esteem'd him-- Did slay this Fortinbras; who by a seal'd compact, Well ratified by law and heraldry, Did forfeit, with his life, all those his lands Which he stood seized of, to the conqueror: Against the which, a moiety competent Was gaged by our king; which had return'd To the inheritance of Fortinbras, Had he been vanquisher; as, by the same covenant, And carriage of the article design'd, His fell to Hamlet. Now, sir, young Fortinbras, Of unimproved mettle hot and full, Hath in the skirts of Norway here and there Shark'd up a list of lawless resolutes, For food and diet, to some enterprise That hath a stomach in't; which is no other-- As it doth well appear unto our state-- But to recover of us, by strong hand And terms compulsatory, those foresaid lands So by his father lost: and this, I take it, Is the main motive of our preparations, The source of this our wat"
+
+    print get_chunked_word_frequency(s,'him', 20)
 
 
 
