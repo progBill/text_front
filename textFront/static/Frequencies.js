8ea43bba290e5e70ddb732ef75461b2c220f8a35
Bill <billebeling@gmail.com>
2014-11-01 08:33:49 -0400

    got new chart work and reduced redundancy in js

--- textFront/static/Frequencies.js
+++ textFront/static/Frequencies.js
@@ -3,16 +3,50 @@ function Frequencies (){ Helper.apply(this, arguments); }
 Frequencies.prototype = Helper.prototype;
 var frequencies = new Frequencies();
 
+Frequencies.prototype.applyFilters = function(){
+    var whiteList = [];
+    var blackList = [];
+    var returnList = [];
+    var lib = store.getLib();
+    if (frequencies.getTextByClass(".whiteList").length > 1){
+         whiteList = frequencies.getTextByClass('.whiteList').replace(/, /g, ',').replace(/\n/g, '').split(',');
+    }
+    if (frequencies.getTextByClass(".blackList").length > 1){
+         blackList = frequencies.getTextByClass('.blackList').replace(/, /g, ',').replace(/\n/g, '').split(',');
+    }
+    for (var w in lib){
+        if (lib.hasOwnProperty(w)){
+            var inWhite=true;
+            var inBlack=false;
+            // see if the list exists, then whether a particular word is on it
+            if (whiteList.length > 0 && whiteList.indexOf(w) === -1) inWhite = false;
+            if (blackList.length > 0 && blackList.indexOf(w) !== -1) inBlack = true;
+            if (inWhite && !inBlack) returnList.push(w);
+        }
+    }
+    return returnList;
+};
+Frequencies.prototype.formatList= function( list ){
+    for (var i in list){
+        i = i + '<br />';
+    }
+};
+Frequencies.prototype.blitElem= function( elem ){
+    frequencies.hideElem('.params');
+    frequencies.hideElem('.display');
+    frequencies.hideElem('#chart');
+
+    frequencies.showElem( elem );
+};
+
 /**
  * displays the list of all tokens with their counts in parens
  */
 Frequencies.prototype.displayAll = function(){
-    frequencies.hideElem('.params');
-    frequencies.showElem('.display');
     var allWords = [];
     var lib = store.getLib();
     for (var w in lib){
-      if (w.prototypeOf(lib)){
+      if (lib.hasOwnProperty(w)){
         allWords.push(w+= ' ( ' + lib[w] + ' )<br />');
         }
         }
@@ -28,39 +62,17 @@ Frequencies.prototype.displayAll = function(){
      });
 
     frequencies.setTextByClass({selector: '.txtDisplay', data:allWords });
+    frequencies.blitElem('.display');
 };
 /**
  * HAPAXES
  * displays list of all singly occuring words
  */
 Frequencies.prototype.displayHapaxes = function(){
-
-    frequencies.hideElem('.params');
-    frequencies.showElem('.display');
-    var lib = store.getLib();
-    var hapaxes = [];
-////BEGIN PARAM PROCESSING
-    var whiteList = [];
-    var blackList = [];
-    if (frequencies.getTextByClass(".whiteList").length > 1){
-         whiteList = frequencies.getTextByClass('.whiteList').replace(/, /g, ',').replace(/\n/g, '').split(',');
-    }
-    if (frequencies.getTextByClass(".blackList").length > 1){
-         blackList = frequencies.getTextByClass('.blackList').replace(/, /g, ',').replace(/\n/g, '').split(',');
-    }
-    for (var w in lib){
-      if (lib.hasOwnPoperty(w)){
-        var inWhite=true;
-        var inBlack=false;
-        // see if the list exists, then whether a particular word is on it
-        if (whiteList.length > 0 && whiteList.indexOf(w) === -1) inWhite = false;
-        if (blackList.length > 0 && blackList.indexOf(w) !== -1) inBlack = true;
-        if (inWhite && !inBlack ){ hapaxes.push(w+='<br />'); }
-    }
-    }
-////END PARAM PROCESSING
-
-    frequencies.setTextByClass({selector:'.txtDisplay', data:hapaxes});
+    var hapaxes = frequencies.applyFilters();
+//    hapaxes = frequencies.formatList( hapaxes );
+    frequencies.setTextByClass({selector:'.txtDisplay', data: hapaxes});
+    frequencies.blitElem('.display');
 };
 
 /**
@@ -72,117 +84,53 @@ Frequencies.prototype.displayLongest= function(){
     frequencies.hideElem('.params');
     frequencies.showElem('.display');
     var lib = store.getLib();
-    var longWords= [];
-////BEGIN PARAMS
-    var whiteList = [];
-    var blackList = [];
-    if (frequencies.getTextByClass(".whiteList").length > 1){
-         whiteList = frequencies.getTextByClass('.whiteList').replace(/, /g, ',').replace(/\n/g, '').split(',');
-    }
-    if (frequencies.getTextByClass(".blackList").length > 1){
-         blackList = frequencies.getTextByClass('.blackList').replace(/, /g, ',').replace(/\n/g, '').split(',');
-    }
-    for (var w in lib){
-      if (lib.hasOwnProperty(w)){
-        var inWhite=true;
-        var inBlack=false;
-        // see if the list exists, then whether a particular word is on it
-        if (whiteList.length > 0 && whiteList.indexOf(w) === -1) inWhite = false;
-        if (blackList.length > 0 && blackList.indexOf(w) !== -1) inBlack = true;
-        if (inWhite && !inBlack ){ longWords.push(w+='<br />'); }
-      }
-    }
-////END PARAMS
+    var longWords= frequencies.applyFilters();
     longWords = longWords.sort(function(a, b){ return b.length-a.length; }).slice(0, num);
     frequencies.setTextByClass({selector:'.txtDisplay',data:longWords});
 };
 Frequencies.prototype.displayChart=function(){
     store.chunk_freq={};
-////BEGIN PARAMS
-    var whiteList = [];
-    var blackList = [];
-    if (frequencies.getTextByClass(".whiteList").length > 1){
-         whiteList = frequencies.getTextByClass('.whiteList').replace(/, /g, ',').replace(/\n/g, '').split(',');
-    }
-    if (frequencies.getTextByClass(".blackList").length > 1){
-         blackList = frequencies.getTextByClass('.blackList').replace(/, /g, ',').replace(/\n/g, '').split(',');
-    }
+    var chartWords = frequencies.applyFilters();
+    frequencies.getJson('/get_word_freq_in_chunk','CHUNKS', chartWords);
 
-////END PARAMS
-    for (var w in whiteList){
-      if (whiteList.hasOwnProperty(w)){
-        //TODO: check this out, it fires when first call is done instead of last call.
-        frequencies.getJson('/get_word_freq_in_chunk','CHUNKS',whiteList[w]);
-      }
-    }
 };
 Frequencies.prototype.makeChart=function(){
-console.log('making chart');
+    frequencies.showElem('#chart');
+    frequencies.hideElem('.params');
+    var chart1 = new Highcharts.Chart({
+        chart: {
+            renderTo: 'chart',
+            type: 'spline',
+        },
+        title: { text: 'Frequencies' },
+        //xAxis: { categories: ['Chunk'] },
+        yAxis: { title: { text: 'Count' } },
+    });
 
-    // Get the context of the canvas element we want to select
-    var ctx = document.getElementById("chart").getContext("2d");
-    var colorOffset = 0;
-    var wordData = [];
-    for (var i in store.chunk_freq){
-      if (store.chunk_freq.hasOwnProperty(i)){
-        wordData.push({
-            label: i,
-            fillColor: "rgba(220,220,"+(10+colorOffset)+",0.2)",
-            strokeColor: "rgba(220,220,"+(10+colorOffset)+ ",1)",
-            pointColor: "rgba(220,220,"+(10+colorOffset)+",1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: store.chunk_freq[i]
-    
-            });
-        colorOffset += 75;
-      }
+    for ( var i in store.chunk_freq ){
+        chart1.addSeries({
+            name:i, data:store.chunk_freq[i]
+        });
     }
-
-    var data = {
-        labels: Array.apply(null, Array(store.chunk_freq[i].length)).map(function() { return ''; }), 
-        datasets: wordData,
-    };
-
-    new Chart(ctx).Line(data);
-
 };
-/**
- * Utilities and Setup 
- */
 
-Frequencies.prototype.paramHapaxes = function(){
-    frequencies.hideElem('.display');
-    frequencies.showElem('.params');
+
+Frequencies.prototype.showParams = function( action ){
+console.log(action);
     frequencies.setTextByClass({selector:'.whiteList', data:''});
     frequencies.setTextByClass({selector:'.blackList', data:''});
-    frequencies.onClickByClass('.jsTaskExecute', frequencies.displayHapaxes);
+    frequencies.onClickByClass('.jsTaskExecute', action);
+    frequencies.blitElem('.params');
 };
-Frequencies.prototype.paramLongest = function(){
-    frequencies.hideElem('.display');
-    frequencies.showElem('.params');
-    frequencies.setTextByClass({selector:'.whiteList', data:''});
-    frequencies.setTextByClass({selector:'.blackList', data:''});
-    frequencies.onClickByClass('.jsTaskExecute', frequencies.displayLongest);
-};
-Frequencies.prototype.paramChart = function(){
-    frequencies.hideElem('.display');
-    frequencies.showElem('.params');
-    frequencies.setTextByClass({selector:'.whiteList', data:''});
-    frequencies.setTextByClass({selector:'.blackList', data:''});
-    frequencies.onClickByClass('.jsTaskExecute', frequencies.displayChart);
-};
-
-function turnTestsOn(){
-    frequencies.onClickByClass('.jsHapaxes', frequencies.paramHapaxes);
-    frequencies.onClickByClass('.jsAll-Tokens', frequencies.displayAll);
-    frequencies.onClickByClass('.jsLong-Words', frequencies.paramLongest);
-    frequencies.onClickByClass('.jsChart', frequencies.paramChart);
-//    frequencies.getJson('/get_word_freq_in_chunk','CHUNKS','and');
-}
 
 /////////  SETUP
+function turnTestsOn(){
+    frequencies.onClickByClass('.jsAll-Tokens', frequencies.displayAll);
+    frequencies.onClickByClass('.jsHapaxes', frequencies.showParams, frequencies.displayHapaxes);
+    frequencies.onClickByClass('.jsLong-Words', frequencies.showParams, frequencies.displayLongest);
+    frequencies.onClickByClass('.jsChart', frequencies.showParams, frequencies.paramChart);
+}
+
 store.subscribe('LIB_READY', turnTestsOn);
 store.subscribe('CHUNKS', frequencies.makeChart);
 frequencies.getJson('/get_word_count', 'LIB_READY');
