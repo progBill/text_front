015fd98db4353857db68131234e1e6c03b7404d6
Bill <billebeling@gmail.com>
2014-11-15 19:47:44 -0500

    added userpage stuff, refactored templates

--- textFront/page_controller.py
+++ textFront/page_controller.py
@@ -4,12 +4,20 @@ from os import listdir, path
 task_dependencies={
     'Home':{
         'js':['Store.js','Helper.js','Home.js'],
+        'sub-menu':[],
     },
     'Frequencies':{
         'js':['Highcharts-4.0.4/js/highcharts.js','Store.js','Helper.js','Frequencies.js'],
-        'task-types':['All Tokens','Hapaxes','Long Words', 'Chart']
+        'sub-menu':['All Tokens','Hapaxes','Long Words', 'Chart']
     },
-#    'Word Locations':{},
+    'User':{
+        'js':[],
+        'sub-menu':[],
+    },
+    'About':{
+        'js':[],
+        'sub-menu':[''],
+    },
 }
 
 
