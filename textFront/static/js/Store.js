f59a503d4a173d2619317eb14d9a9fffd99d9c13
Bill <billebeling@gmail.com>
2014-11-21 19:22:12 -0500

    converted js objects to .create instead of new <obj>

--- textFront/static/js/Store.js
+++ textFront/static/js/Store.js
@@ -1,39 +1,30 @@
-function Store(){};
-Store.prototype.lib={};
-Store.prototype.topics={};
-Store.prototype.subscribe=function(topic, cb){
-    if (!this.topics[topic]){ this.topics[topic] = { queue: [] }; };
-    var idx = this.topics[topic].queue.push(cb) - 1;
-};
-Store.prototype.remove=function(){delete this.topics[topic].queue[idx];};
-Store.prototype.publish=function(topic, info){
-    if (!this.topics[topic]) return;
-    var cbs = this.topics[topic].queue;
-    cbs.forEach(function(cb) {
-        cb(info || {});
-    });
-};
-Store.prototype.getLib=function(){
-    return this.lib;
-};
-Store.prototype.getList=function(){
-    var word_list = [];
-    for (var i in this.lib ){
-        word_list.push( i );
-    }
-    return word_list;
-};
-Store.prototype.getter=function(x){
-    return this.lib[x];
-};
-Store.prototype.setter=function(x,y){
-    if(!this.lib){ lib={}; };
-     this.lib[x]=y;
-};
-Store.prototype.out=function(x){
-    console.log(x);
+var Store = {
+    lib: {},
+    topics: {},
+    subscribe: function (topic, cb) {
+        if (!this.topics[topic]) { this.topics[topic] = { queue: [] }; }
+        var idx = this.topics[topic].queue.push(cb) - 1;
+    },
+    remove: function(topic, idx){ delete this.topics[topic].queue[idx]; },
+    publish: function(topic, info){
+        if (!this.topics[topic]) return;
+        var cbs = this.topics[topic].queue;
+        cbs.forEach(function(cb) { cb(info || {}); });
+    },
+    getLib: function(){ return this.lib; },
+    getList: function(){
+        var word_list = [];
+        for (var i in this.lib ){
+            word_list.push( i );
+          }
+        return word_list;
+    },
+    getter: function(x){ return this.lib[x]; },
+    setter: function(x,y){
+            if(typeof(lib)==="undefined"){ lib={}; }
+         this.lib[x]=y;
+    },
 };
 
-var store = new Store();
-
+var store = Object.create(Store);
 
